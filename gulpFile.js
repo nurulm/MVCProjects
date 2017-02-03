7d7d66505de49ad53ac34906f048f3983ce67a66
nurulm <nurul.ameen@gmail.com>
2017-02-03 23:20:35 +0530

    added

--- gulpFile.js
+++ gulpFile.js
@@ -8,7 +8,9 @@ var minifyimage = require('gulp-imagemin');
 //contactinating all js files - bundling
 gulp.task('BundleJavaScript',function(){
 
-     return gulp.src(["./bower_components/angular/angular.js", "./bower_components/jquery/dist/jquery.js", "./bower_components/underscore/underscore.js","./bower_components/bootstrap/dist/js/bootstrap.js","./bower_components/jquery-ui/jquery-ui.js/"])
+     //return gulp.src(["./bower_components/angular/angular.js", "./bower_components/jquery/dist/jquery.js", "./bower_components/underscore/underscore.js","./bower_components/bootstrap/dist/js/bootstrap.js","./bower_components/jquery-ui/jquery-ui.js/"])
+     return gulp.src(["./bower_components/jquery/dist/jquery.js","./bower_components/bootstrap/dist/js/bootstrap.js",
+               "./bower_components/angular/angular.js","./bower_components/jquery-ui/jquery-ui.js/","./bower_components/underscore/underscore-min.js"])
      .pipe(concat('all.js'))
      .pipe(iife())
      .pipe(uglify())
