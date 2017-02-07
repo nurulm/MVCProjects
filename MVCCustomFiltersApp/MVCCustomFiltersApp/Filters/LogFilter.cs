e86da1d0202a98b2cd54b4db5d46f53a4854b571
nurulm <nurul.ameen@gmail.com>
2017-02-07 22:22:17 +0530

    custom filters project added

---
+++ MVCCustomFiltersApp/MVCCustomFiltersApp/Filters/LogFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace MVCCustomFiltersApp.Filters
+{
+    public class LogFilter : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string cname = filterContext.RouteData.Values["controller"].ToString();
+
+            string aname = filterContext.RouteData.Values["action"].ToString();
+
+            string datetimeinfo = DateTime.Now.ToString();
+
+            string info = "Controller :" + cname + "  Action:" + aname + "  DateTime:" + datetimeinfo;
+
+            StreamWriter sw = File.AppendText(System.Web.HttpContext.Current.Server.MapPath("/Files/UserLogs.txt"));
+
+            sw.WriteLine(info);
+            sw.WriteLine("=========================================================================================");
+            sw.Close();
+        }
+    }
+}
