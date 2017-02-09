36427d2d70b1aed009939280197157f095023526
nurulm <nurul.ameen@gmail.com>
2017-02-09 20:53:13 +0530

    mobile app dev projects added

---
+++ MVCMobileAppAdaptiveApproach/MVCMobileAppAdaptiveApproach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCMobileAppAdaptiveApproach.MvcApplication" Language="C#" %>
---
+++ MVCMobileAppDisplayModeApproach/MVCMobileAppDisplayModeApproach/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMobileAppDisplayModeApproach.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index1()
+        {
+            if(Request.Browser.IsMobileDevice)
+            {
+                ViewBag.minfo = "Mobile View Data";
+                return View("mobileview");
+            }
+            else
+            {
+                ViewBag.dinfo = "Desktop View Data";
+                return View("desktopview");
+            }
+        }
+    }
+}
