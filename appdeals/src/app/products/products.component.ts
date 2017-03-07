396d3dd29f56c8d93e16310f6b42a4728efbbf34
nurulm <nurul.ameen@gmail.com>
2017-03-07 22:38:06 +0530

    files addded

--- appdeals/src/app/products/products.component.ts
+++ appdeals/src/app/products/products.component.ts
@@ -1,17 +1,32 @@
 import {Component} from '@angular/core';
 import {ProductsService} from './products.service';
 
+
 @Component({
     selector:"appdeal-product",
     templateUrl:"./products.component.html",
     providers:[ProductsService]
 })
-
 export class ProductComponent{
     
     constructor(private ProductSVC:ProductsService){
-
+        this.getProducts();
+    }
+    products:any;
+    getProducts(){
+        this.ProductSVC.GetProducts()
+        .subscribe(res=>{
+            this.products = res;
+        });
     }
 
-    Products = this.ProductSVC.GetProducts();
+    addProduct(){
+        this.ProductSVC.AddProduct();
+    }
+
+    search(){
+        this.ProductSVC.SearchProduct();
+    }
+
+    //Products = this.ProductSVC.GetProducts();
 }
--- appdeals/src/app/products/products.service.ts
+++ appdeals/src/app/products/products.service.ts
@@ -1,13 +1,26 @@
 import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Observable} from 'rxjs';
+import 'rxjs';
 
 @Injectable()
 export class ProductsService{
+
+    constructor (private http:Http){
+
+    }
     
-    GetProducts()
+    GetProducts():Observable<any>
     {
-        return [{Name:"IPhone", Model:"7s"},{Name:"Samsung", Model:"S7"}];
+        return this.http.get('./src/api/products.json')
+                .map((res)=>res.json())
+                .catch((error)=>error.json());
+        //return [{Name:"IPhone", Model:"7s"},{Name:"Samsung", Model:"S7"}];
     }
 
+    SearchProduct(){
+
+    }
     AddProduct()
     {
 
